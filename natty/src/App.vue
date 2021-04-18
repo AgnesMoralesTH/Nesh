bf0ae025b6746c1460ebe9f01947a5efff790799
blue-jeans <programadeamazonas@gmail.com>
2021-04-18 17:29:35 +0700

    first commit

---
+++ natty/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="nav">
+    <TheNavigation />
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+   margin: 0 auto; 
+    text-align: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<script>
+import TheNavigation from './components/TheNavigation'
+
+export default{
+  components: {
+    TheNavigation
+  }
+}
+</script>
+
---
+++ natty/src/components/TheNavigation.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav id="nav">
+        <p class="logo">Natty</p>
+
+        <ul class="nav-links">
+            <li class="links">HOME</li>
+            <li class="links">SHOP</li>
+            <li class="links">CONTACT</li>
+            <li class="links">INFO</li>
+            
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+#nav{
+    display:flex;
+    width: 500px;
+    height: 100px;
+     display: flex;
+    justify-content: space-between;
+}
+#nav a{
+    font-weight: bold;
+    color: black;
+    text-decoration: none;
+}
+nav a.active{
+    color: #42b983;
+}
+.nav-links{
+    display: flex;
+}
+.links{
+    padding-right: 20px;
+    list-style: none;
+}
+.links:hover{
+    text-decoration: underline;
+}
+</style>
